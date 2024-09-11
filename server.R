19024da317055843b7dc3130af591aad3f052b6a
JBPOST2 <jbpost2@ncsu.edu>
2024-09-11 10:39:24 -0400

    Move data collection out of server - update later

--- server.R
+++ server.R
@@ -13,23 +13,23 @@ library(tidycensus)
 
 my_key <- "e267f117801b2ef741e54620602b0903c5f4d3c8"
 
+#just for now!
+PA_data <- get_pums(
+  variables = c("JWMNP", "PINCP"), #travel and income, likely need to subset out 0 travel time people... dont' worry about too much of that now!
+  state = "PA",
+  recode = TRUE,
+  year = 2022,
+  survey = "acs1",
+  key = my_key
+  ) |>
+  mutate(JWMNP = as.numeric(JWMNP), PINCP = as.numeric(PINCP)) |>
+  filter(PINCP > 0)
 
 function(input, output, session) {
   sample_data <- reactiveValues(my_sample = NULL)
   
   observeEvent(input$sample, {
-    data <- get_pums(
-      variables = c("JWMNP", "PINCP"), #travel and income, likely need to subset out 0 travel time people... dont' worry about too much of that now!
-      state = "PA",
-      recode = TRUE,
-      year = 2022,
-      survey = "acs1",
-      key = my_key
-    ) |>
-      mutate(JWMNP = as.numeric(JWMNP), PINCP = as.numeric(PINCP)) |>
-      filter(PINCP > 0)
-    
-    sample_data$my_sample <- data[sample(1:nrow(data),100), ] 
+    sample_data$my_sample <- PA_data[sample(1:nrow(PA_data),100), ] 
       
   })
   
